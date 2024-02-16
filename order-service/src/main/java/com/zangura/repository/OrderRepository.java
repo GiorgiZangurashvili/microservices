48de1dd4ed9fa156958c260078934a8e5558878c
GiorgiZangurashvili <giozangurashvili225@gmail.com>
2024-02-17 00:10:43 +0400

    Add Product and Order Service

---
+++ order-service/src/main/java/com/zangura/repository/OrderRepository.java
@@ -0,0 +1,9 @@
+package com.zangura.repository;
+
+import com.zangura.domain.Order;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+@Repository
+public interface OrderRepository extends JpaRepository<Order, Long> {
+}
---
+++ order-service/src/main/java/com/zangura/service/OrderService.java
@@ -0,0 +1,26 @@
+package com.zangura.service;
+
+import com.zangura.domain.Order;
+import com.zangura.dto.OrderRequest;
+import com.zangura.mapper.OrderLineItemsMapper;
+import com.zangura.repository.OrderRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.stereotype.Service;
+
+import java.util.UUID;
+
+@Service
+@RequiredArgsConstructor
+public class OrderService {
+    private final OrderRepository orderRepository;
+    private final OrderLineItemsMapper orderLineItemsMapper;
+
+    public void placeOrder(OrderRequest orderRequest) {
+        Order order = new Order();
+        order.setOrderNumber(UUID.randomUUID().toString());
+        order.setOrderLineItemsList(orderLineItemsMapper
+                .mapDtosToEntities(orderRequest.getOrderLineItemsDTOList()));
+
+        orderRepository.save(order);
+    }
+}

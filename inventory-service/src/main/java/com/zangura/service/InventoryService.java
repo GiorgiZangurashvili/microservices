56585bd5828d9df88e04cf12e935dbf6e9f6088d
GiorgiZangurashvili <giozangurashvili225@gmail.com>
2024-03-03 03:31:29 +0400

    Implement Circuitbreaker using Resilience4j.

--- inventory-service/src/main/java/com/zangura/service/InventoryService.java
+++ inventory-service/src/main/java/com/zangura/service/InventoryService.java
@@ -3,6 +3,8 @@ package com.zangura.service;
 import com.zangura.dto.InventoryResponse;
 import com.zangura.repository.InventoryRepository;
 import lombok.RequiredArgsConstructor;
+import lombok.SneakyThrows;
+import lombok.extern.slf4j.Slf4j;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 
@@ -11,12 +13,14 @@ import java.util.stream.Collectors;
 
 @Service
 @RequiredArgsConstructor
+@Slf4j
 public class InventoryService {
 
     private final InventoryRepository inventoryRepository;
 
     @Transactional(readOnly = true)
     public List<InventoryResponse> isInStock(List<String> skuCodes) {
+        log.info("Checking if items are in stock");
         return inventoryRepository.findBySkuCodeIn(skuCodes)
                 .stream()
                 .map(inventory -> InventoryResponse
--- order-service/src/main/java/com/zangura/service/OrderService.java
+++ order-service/src/main/java/com/zangura/service/OrderService.java
@@ -25,7 +25,7 @@ public class OrderService {
     private final OrderLineItemsMapper orderLineItemsMapper;
     private final WebClient.Builder webClientBuilder;
 
-    public void placeOrder(OrderRequest orderRequest) {
+    public String placeOrder(OrderRequest orderRequest) {
         Order order = new Order();
         order.setOrderNumber(UUID.randomUUID().toString());
         order.setOrderLineItemsList(orderLineItemsMapper
@@ -52,6 +52,7 @@ public class OrderService {
 
         if (isInStock) {
             orderRepository.save(order);
+            return "Order placed successfully";
         } else {
             throw new ItemNotInStockException();
         }
